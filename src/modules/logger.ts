a5f9457c4f872d590f302b023b3b4be0ee8feb3e
W0LFB0MB <hopyplays@gmail.com>
2023-03-28 15:20:46 +0100

    Updated logging system

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -9,18 +9,39 @@ export enum LogSeverity {
 	FATAL = 'FATAL',
 }
 
-export class Log {
+class Log {
 	public readonly timestamp = Date.now();
 	public readonly severity: LogSeverity;
 	public readonly message: string;
+	public readonly stack: string | undefined;
 
-	constructor(severity: LogSeverity, message: string) {
+	constructor(severity: LogSeverity, message: string, stack: string | undefined) {
 		this.severity = severity;
 		this.message = message;
+		this.stack = stack;
 	}
 
 	toString(): string {
-		return `${this.timestamp} [${this.severity}] : ${this.message}`;
+		const extraSpacing = ' '.repeat(5 - this.severity.length);
+		return `${this.timestamp} [${this.severity}]${extraSpacing} : ${this.message}`;
+	}
+
+	toConsoleString(): string {
+		const extraSpacing = ' '.repeat(5 - this.severity.length);
+		const consoleReset = '\x1b[0m';
+
+		let severityColor = '';
+
+		if (this.severity === LogSeverity.TRACE) severityColor = '\x1b[0;90m';  // Grey FG
+		if (this.severity === LogSeverity.DEBUG) severityColor = '\x1b[0;90m';  // White FG
+		if (this.severity === LogSeverity.INFO) severityColor = '\x1b[0;97m';   // White FG
+		if (this.severity === LogSeverity.WARN) severityColor = '\x1b[0;93m';   // Yellow FG
+		if (this.severity === LogSeverity.ERROR) severityColor = '\x1b[0;91m';  // Red FG
+		if (this.severity === LogSeverity.FATAL) severityColor = '\x1b[41;39m';  // Red FG Black BG
+
+		const stackString = this.stack ? `\r\n${this.stack}` : '';
+
+		return `\x1b[0;90m${this.timestamp} ${severityColor}[${this.severity}]${consoleReset}${extraSpacing} : ${this.message}${consoleReset}${stackString}`;
 	}
 }
 
@@ -38,19 +59,19 @@ export default class Logger {
 		}
 	}
 
-	public static log(severity: LogSeverity, message: string) {
-		const log = new Log(severity, message);
+	public static log(severity: LogSeverity, message: string, stack: string | undefined): void {
+		const log = new Log(severity, message, stack);
 		this.logs.push(log);
 		fs.appendFileSync(`${this.directory}/latest.log`, log.toString() + '\r\n');
-		console.log(log.toString());
+		console.log(log.toConsoleString());
 	}
 
-	public static trace = (message: string) => this.log(LogSeverity.TRACE, message);
-	public static debug = (message: string) => this.log(LogSeverity.DEBUG, message);
-	public static info = (message: string) => this.log(LogSeverity.INFO, message);
-	public static warn = (message: string) => this.log(LogSeverity.WARN, message);
-	public static error = (message: string) => this.log(LogSeverity.ERROR, message);
-	public static fatal = (message: string) => this.log(LogSeverity.FATAL, message);
+	public static trace = (message: string): void => this.log(LogSeverity.TRACE, message, undefined);
+	public static debug = (message: string): void => this.log(LogSeverity.DEBUG, message, undefined);
+	public static info = (message: string): void => this.log(LogSeverity.INFO, message, undefined);
+	public static warn = (message: string): void => this.log(LogSeverity.WARN, message, undefined);
+	public static error = (message: string, trace: string | undefined): void => this.log(LogSeverity.ERROR, message, trace);
+	public static fatal = (message: string, trace: string | undefined): void => this.log(LogSeverity.FATAL, message, trace);
 
 	public static toString(): string {
 		let logString = '';
@@ -62,8 +83,16 @@ export default class Logger {
 		return logString;
 	}
 
-	public static saveLogs() {
-		const logString = `\r\n## START OF LOGS - ${this.instantiated} ##\r\n` + this.toString() + `\r\n## END OF LOGS - ${Date.now()} ##\r\n`;
+	public static saveLogs(): void {
+		//const logString = `\r\n## START OF LOGS - ${this.instantiated} ##\r\n` + this.toString() + `\r\n## END OF LOGS - ${Date.now()} ##\r\n`;
+
+		let logString = '[\r\n';
+
+		this.logs.forEach(log => {
+			logString += '	' + JSON.stringify(log) + ',\r\n';
+		});
+
+		logString += ']';
 
 		fs.writeFileSync(`${this.directory}/latest.log`, logString);
 		fs.writeFileSync(`${this.directory}/${this.instantiated}.log`, logString);
