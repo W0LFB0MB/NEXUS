84217d638e3276db27b457271aa9f52493e1bddc
W0LFB0MB <hopyplays@gmail.com>
2024-01-13 07:20:20 +0000

    added track and queue loop functionality to one commans

--- src/commands/interaction/loop.ts
+++ src/commands/interaction/loop.ts
@@ -1,13 +1,34 @@
-import { ApplicationCommandType, ChatInputCommandInteraction } from 'discord.js';
+import { ApplicationCommandOptionType, ApplicationCommandType, ChatInputCommandInteraction } from 'discord.js';
 import { LoopType } from '../../modules/subscription.js';
 
 import musicMiddleware from '../../modules/middleware/musicMiddleware.js';
 
 export default {
 	name: 'loop',
-	description: 'Loops the currently playing track',
+	description: 'Set loop mode',
 	type: ApplicationCommandType.ChatInput,
-	options: [],
+	options: [
+		{
+			name: 'mode',
+			type: ApplicationCommandOptionType.String,
+			description: 'Loop mode',
+			required: true,
+			choices: [
+				{
+					name: 'off',
+					value: 'off'
+				},
+				{
+					name: 'track',
+					value: 'track'
+				},
+				{
+					name: 'queue',
+					value: 'queue'
+				},
+			]
+		}
+	],
 	global: true,
 	async execute(interaction: ChatInputCommandInteraction) {
 		const { default: Bot } = await import('../../bot.js');
@@ -16,13 +37,16 @@ export default {
 		musicMiddleware(interaction, async () => {
 			if (!subscription) return interaction.reply({ content: 'Not playing in this server!', ephemeral: true });
 
-			if (subscription.loop !== LoopType.track) {
-				subscription.loop = LoopType.track;
-			} else {
+			if (interaction.options.getString('mode') === 'off') {
 				subscription.loop = LoopType.none;
+				interaction.reply({ content: ('Loop disabled.'), ephemeral: false });
+			} else if (interaction.options.getString('mode') === 'track') {
+				subscription.loop = LoopType.track;
+				interaction.reply({ content: ('Track loop enabled.'), ephemeral: false });
+			} else if (interaction.options.getString('mode') === 'queue') {
+				subscription.loop = LoopType.queue;
+				interaction.reply({ content: ('Queue loop enabled.'), ephemeral: false });
 			}
-
-			interaction.reply({ content: (subscription.loop === LoopType.track ? 'Song looped!' : 'Loop stopped.'), ephemeral: false });
 		});
 	}
 };
